99c320a71a030a2bb3b318a51cb7021451748615
SLYtiger16 <36513817+SLYtiger16@users.noreply.github.com>
2018-02-15 16:06:05 -0600

    Add files via upload

--- www/js/index.js
+++ www/js/index.js
@@ -5,6 +5,8 @@ var app = {
   start: document.getElementById( 'startBtn' ),
   clock: document.getElementById( 'clock' ),
   timer: document.getElementById( 'timer' ),
+  startedAt: document.getElementById( 'startedAt' ),
+  info: $( '.info' ),
   // Application Constructor
   initialize: function () {
     this.bindEvents();
@@ -18,10 +20,16 @@ var app = {
     document.addEventListener( 'deviceready', this.onDeviceReady, false );
     document.addEventListener( 'online', this.online, false );
     document.addEventListener( 'offline', this.offline, false );
+    $( 'html' ).on( 'click', '#local', function () {
+      app.local();
+    } );
   },
   onDeviceReady: function () {
     console.log( 'deviceready: ' + app.timeDate() );
+    let main = $( 'main' ).height();
+    $( '#tempo' ).height( main - 240 );
     app.start.addEventListener( 'click', app.tempoStart );
+    app.info.on( 'click', app.infoClick );
     app.clockStart();
   },
   load: function () {
@@ -33,6 +41,32 @@ var app = {
   offline: function () {
     console.log( 'offline: ' + app.timeDate() );
   },
+  infoClick: function () {
+    $( '#app, #about' ).toggle();
+  },
+  local: function ( reachability ) {
+    let networkState = navigator.connection.type;
+
+    let states = {};
+    states[ Connection.UNKNOWN ] = 'Unknown connection';
+    states[ Connection.ETHERNET ] = 'Ethernet connection';
+    states[ Connection.WIFI ] = 'WiFi connection';
+    states[ Connection.CELL_2G ] = 'Cell 2G connection';
+    states[ Connection.CELL_3G ] = 'Cell 3G connection';
+    states[ Connection.CELL_4G ] = 'Cell 4G connection';
+    states[ Connection.CELL ] = 'Cell generic connection';
+    states[ Connection.NONE ] = 'No network connection';
+
+    app.notify( 'Connection type: ' + states[ networkState ] );
+    console.log( 'Connection type: ' + states[ networkState ] );
+  },
+  notify: function ( x ) {
+    alert( 'test' );
+    cordova.plugins.notification.local.registerPermission( function ( granted ) {
+      console.log( 'Permission has been granted: ' + granted );
+    } );
+    cordova.plugins.notification.local.schedule( { title: 'My first notification', text: x, foreground: true } );
+  },
   timeDate: function () {
     let x = new Date(),
       d = x.toLocaleDateString(),
@@ -42,15 +76,20 @@ var app = {
   tempoStart: function () {
     app.play();
     app.timerFunc.start();
+    $( '#timer' ).addClass( 'deep-orange-text text-lighten-1' );
     app.start.removeEventListener( 'click', app.tempoStart );
     app.start.addEventListener( 'click', app.tempoStop );
+    $( '#start' ).removeClass( 'green' ).addClass( 'red' );
   },
   tempoStop: function () {
     app.stop();
     app.start.innerHTML = "START";
     app.timer.innerHTML = "00:00:00";
+    $( '#timer' ).removeClass( 'deep-orange-text text-lighten-1' );
+    app.startedAt.innerHTML = "---";
     app.start.removeEventListener( 'click', app.tempoStop );
     app.start.addEventListener( 'click', app.tempoStart );
+    $( '#start' ).removeClass( 'red' ).addClass( 'green' );
     clearInterval( app.timerInterval );
   },
   clockStart: function () {
@@ -63,7 +102,7 @@ var app = {
         formatLength: 'short',
         selector: 'time'
       } );
-    }, 500 );
+    }, 1000 );
   },
   play: function () {
     if ( typeof audio.loop == 'boolean' ) {
@@ -102,9 +141,11 @@ var app = {
         app.timer.innerHTML = text;
       }, 1000 );
       navigator.globalization.dateToString( new Date(), function ( date ) {
-        app.start.innerHTML = "STOP<br/><small class='small'><i>Started at: " + date.value + "</i></small>";
+        app.start.innerHTML = "STOP";
+        app.startedAt.innerHTML = "Started at: " + date.value;
       }, function () {
-        app.start.innerHTML = "STOP<br/><small class='small'><i>Started at: Error</i></small>";
+        app.start.innerHTML = "STOP";
+        app.startedAt.innerHTML = "Started at: ERROR";
       }, {
         formatLength: 'full',
         selector: 'time'
